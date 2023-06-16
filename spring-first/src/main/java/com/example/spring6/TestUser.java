579fe16d8490b8816100f05afab2c1418246864d
Kuro1Fury <skycoder0820@gmail.com>
2023-06-16 15:33:47 +0800

    reflection

--- spring-first/src/main/java/com/example/spring6/TestUser.java
+++ spring-first/src/main/java/com/example/spring6/TestUser.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.lang.reflect.InvocationTargetException;
+
 public class TestUser {
 
     @Test
@@ -18,5 +20,15 @@ public class TestUser {
         // Call methods on the bean and test the results
         System.out.print("2:");
         user.add();
+    }
+
+    // Reflection construction
+    @Test
+    public void testUserObject1() throws Exception {
+        // Get class object
+        Class<?> clazz = Class.forName("com.example.spring6.user");
+        // Call constructor
+        User user = (User) clazz.getDeclaredConstructor().newInstance();
+        System.out.println(user);
     }
 }
--- spring-first/src/main/java/com/example/spring6/User.java
+++ spring-first/src/main/java/com/example/spring6/User.java
@@ -2,6 +2,11 @@ package com.example.spring6;
 
 public class User {
 
+    // Zero Constructor
+    public User() {
+        System.out.println("0: Zero Constructor Executed");
+    }
+
     public void add() {
         System.out.println("add...");
     }
