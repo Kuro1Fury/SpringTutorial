5ba0e9739da01d91cd9e5ac9509722028ec5ca34
Kuro1Fury <skycoder0820@gmail.com>
2023-06-19 17:32:01 +0800

    component scan

---
+++ spring6-ioc-annotation/src/main/java/com/example/spring6/bean/User.java
@@ -0,0 +1,13 @@
+package com.example.spring6.bean;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+//@Component(value = "user") // equal to <bean id="user" class="com.example.spring6.bean.User"/>
+//@Repository
+//@Service
+@Controller
+public class User {
+}
