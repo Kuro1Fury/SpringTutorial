4051949f91a80f384edcdeea6f688a2e74ddb284
Kuro1Fury <skycoder0820@gmail.com>
2023-06-21 15:23:05 +0800

    fixed bug

--- spring6-aop/src/main/java/com/example/spring6/aop/annoaop/TestAop.java
+++ spring6-aop/src/main/java/com/example/spring6/aop/annoaop/TestAop.java
@@ -3,12 +3,13 @@ package com.example.spring6.aop.annoaop;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestAop {
 
     @Test
     public void testAdd() {
-        ApplicationContext context = new AnnotationConfigApplicationContext("bean.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
         Calculator calculator = context.getBean(Calculator.class);
         calculator.add(2, 3);
     }
