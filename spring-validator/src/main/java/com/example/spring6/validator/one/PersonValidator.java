305f85c9cb43e9f88d2368ed50ea1dfe28b7e774
Kuro1Fury <skycoder0820@gmail.com>
2023-06-23 13:29:59 +0800

    validator interface

---
+++ spring-validator/src/main/java/com/example/spring6/validator/one/PersonValidator.java
@@ -0,0 +1,25 @@
+package com.example.spring6.validator.one;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+public class PersonValidator implements Validator {
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return Person.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        ValidationUtils.rejectIfEmpty(errors, "name", "name.empty", "Name is empty");
+
+        Person p = (Person) target;
+        if (p.getAge() < 0) {
+            errors.rejectValue("age", "age.value.negative", "Age is negative");
+        } else if (p.getAge() > 110) {
+            errors.rejectValue("age", "age.value.great", "Age is too great");
+        }
+
+    }
+}
