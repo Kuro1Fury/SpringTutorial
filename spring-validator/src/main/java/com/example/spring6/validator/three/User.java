17223ec86452f791317981cf9c0a9f4c584e0972
Kuro1Fury <skycoder0820@gmail.com>
2023-06-23 14:01:44 +0800

    validation based on method

---
+++ spring-validator/src/main/java/com/example/spring6/validator/three/User.java
@@ -0,0 +1,41 @@
+package com.example.spring6.validator.three;
+
+import jakarta.validation.constraints.*;
+
+public class User {
+
+    @NotNull
+    private String name;
+
+    @Min(0)
+    @Max(110)
+    private int age;
+
+    @Pattern(regexp = "^1(3|4|5|7|8)\\d{9}$",message = "format error")
+    @NotBlank(message = "phone number cannot be empty")
+    private String phone;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
